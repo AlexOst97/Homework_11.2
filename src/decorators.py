8da23ee294c20d11d52f3e8fd51f2e590b612261
unknown <sanya@example.com>
2024-12-02 23:26:50 +0700

    add_4

--- src/decorators.py
+++ src/decorators.py
@@ -1,7 +1,7 @@
 from functools import wraps
 
 
-def log(filename="mylog.txt"):
+def log(filename=None):
     """декоратор, который будет автоматически логировать начало и конец выполнения функции,
     а также ее результаты или возникшие ошибки."""
 
@@ -9,16 +9,23 @@ def log(filename="mylog.txt"):
         @wraps(function)
         def wrapper(*args, **kwargs):
             try:
-                result = function(*args, **kwargs)
-                print(f"{function.__name__} ok")
-                with open("record_result.txt", "a") as file:
-                    file.write(f"{function.__name__} ok - result: {result}\n")
+                function(*args, **kwargs)
+                result = f"{function.__name__} ok"
+                if filename is None:
+                    print(f"{result}")
+                elif filename is not None:
+                    with open("record_result.txt", "a", encoding="utf-8") as file:
+                        file.write(f"{result}\n")
+                return result
+
             except Exception as e:
-                print(f"{function.__name__} error: {e}. Inputs: {args}, {kwargs}")
-                with open("record_result.txt", "a") as file:
-                    file.write(
-                        f"{function.__name__} error: {e}. Inputs: {args}, {kwargs}\n"
-                    )
+                result_e = f"{function.__name__} error: {e}. Inputs: {args}, {kwargs}"
+                if filename is None:
+                    print(f"{result_e}")
+                elif filename is not None:
+                    with open("record_result.txt", "a", encoding="utf-8") as file:
+                        file.write(f"{result_e}\n")
+                return result_e
 
         return wrapper
 
@@ -30,4 +37,4 @@ def my_function(x, y):
     return x / y
 
 
-my_function(4, 0)
+my_function(1, 0)
